3531e18f4b1662d936d350d47d6de1ec9bfeecc0
Alex <amheller@gmail.com>
2020-12-08 16:38:44 -0800

    Initial Commit

---
+++ GoTHouses/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GoTHouses
+//
+//  Created by Alex Heller on 12/8/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
